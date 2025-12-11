4d3e4435cc6eeca9b0cd5749da22ea1b6b321f8d
Reem99-sina <reemsina2@gmail.com>
2025-12-11 13:45:06 +0200

    cleanup secrets

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import Stripe from "stripe";
+require('dotenv').config();
+const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY
+);
+
+const app = express();
+app.use(express.json());
+
+app.post("/api/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "Product Name",
+          },
+          unit_amount: 2000, // $20.00
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "https://yourdomain.com/success",
+    cancel_url: "https://yourdomain.com/cancel",
+  });
+  res.json({ url: session.url, id: session.id });
+});
+
+app.listen(3000);
